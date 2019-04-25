55f665dad7ee190cea3b93a586cef55ae3779574
Kish Siva <rodes88@msn.com>
2019-04-25 01:23:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,26 +49,17 @@ class Song
   end
   
   def self.new_from_filename(new_name)
-    #name_array = new_name.split(Regexp.union([" - ", "."]))
-    #song = new_by_name(name_array[1])
-    #song.artist_name = name_array[0]
-    #song
+    name_array = new_name.split(Regexp.union([" - ", "."]))
+    song = new_by_name(name_array[1])
+    song.artist_name = name_array[0]
+    song
   end
   
   def self.create_from_filename(new_name)
-    #name_array = new_name.split(Regexp.union([" - ", "."]))
-    #song = find_or_create_by_name(name_array[1])
-    #song.artist_name = name_array[0]
-    #song
-  end
-  
-  describe '.create_from_filename' do
-    it 'initializes and saves a song and artist_name based on the filename format' do
-      song = Song.create_from_filename("Thundercat - For Love I Come.mp3")
-      song_match = Song.find_by_name("For Love I Come")
-      expect(song_match.name).to eq("For Love I Come")
-      expect(song_match.artist_name).to eq("Thundercat")
-    end
+    name_array = new_name.split(Regexp.union([" - ", "."]))
+    song = find_or_create_by_name(name_array[1])
+    song.artist_name = name_array[0]
+    song
   end
 
   def self.destroy_all
